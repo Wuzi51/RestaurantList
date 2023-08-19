5c99dbfba3507b2c0598081e834048b3ff8ad545
Richy <wuzi50115@gmail.com>
2023-08-19 22:32:29 +0800

    build route & controller setup

--- app.js
+++ app.js
@@ -3,7 +3,20 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('express app for restaurant')
+  res.send('express app for restaurants')
+})
+
+app.get('/', (req, res) => {
+  res.redirect('/restaurants')
+})
+
+app.get('/restaurant', (req, res) => {
+  res.send('listen restaurants')
+})
+
+app.get('/restaurant/:id', (req, res) => {
+  const id = req.params.id
+  res.send(`read restaurant: ${id}`)
 })
 
 app.listen(port, () => {
